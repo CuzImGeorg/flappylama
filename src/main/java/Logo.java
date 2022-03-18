d12948ce664fbcca8fc3ed28232ff431430424f1
schne <schnecht555@gmail.com>
2022-03-18 11:18:23 +0100

    yeet

--- src/main/java/Logo.java
+++ src/main/java/Logo.java
@@ -8,9 +8,9 @@ public class Logo extends aMovingObject{
     Image img;
 
     public Logo(){
-        super(50,200,100,80);
+        super(50,200,150,80);
         try {
-            img = ImageIO.read(new File("src/main/java/logo2.png"));
+            img = ImageIO.read(new File("src/main/java/flappylama.png"));
         } catch (IOException ex) {
 
         }
